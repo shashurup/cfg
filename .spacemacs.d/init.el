00ea3dd327841b0a6c4ef9f5d93bbbbe652fb39c
Georgy Kibardin <georgy@kibardin.name>
2020-04-16 11:11:06 +0300

    Misc improvements

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -9,7 +9,7 @@ values."
    '(
       python
       clojure
-      (shell :variables shell-default-shell 'shell)
+      (shell :variables shell-default-shell 'eshell)
       yaml
       html
       helm
@@ -28,8 +28,13 @@ values."
       rust
       pdf-tools
       csharp
+      ipython-notebook
+      docker
+      go
      )
-   dotspacemacs-additional-packages '(org-jira)))
+   dotspacemacs-additional-packages '(russian-holidays
+                                      vlf
+                                      pyenv-mode-auto)))
 
 (defun dotspacemacs/init ()
   "Initialization function.
@@ -49,7 +54,7 @@ values."
                                          :width normal
                                          :powerline-scale 1.1))
           ((eq system-type 'gnu/linux) '("Monospace"
-                                         :size 12
+                                         :size 19
                                          :weight normal
                                          :width normal
                                          :powerline-scale 1.1)))
@@ -64,6 +69,7 @@ before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
   (setq ispell-dictionary "english"
         calendar-week-start-day 1
+        calendar-date-style "european"
         calendar-day-name-array ["Вс" "Пн" "Вт" "Ср" "Чт" "Пт" "Сб"]
         calendar-month-name-array ["Январь" "Февраль" "Март" "Апрель" "Май"
                                    "Июнь" "Июль" "Август" "Сентябрь"
@@ -79,10 +85,8 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
   (setq powerline-default-separator nil)
-  (setq jiralib-url "https://jira.lpr.jet.msk.su")
-  (setq org-jira-working-dir "~/jet/org")
-  (setq auth-sources '(default))
   (setq user-full-name "Георгий Кибардин")
+  (setq auth-sources '(default))
   (evil-define-key 'normal cider-repl-mode-map
     (kbd "C-j") 'cider-repl-next-input
     (kbd "C-k") 'cider-repl-previous-input
@@ -90,6 +94,19 @@ you should place your code here."
   (evil-define-key 'insert cider-repl-mode-map
     (kbd "C-j") 'cider-repl-next-input
     (kbd "C-k") 'cider-repl-previous-input)
+
+  (evil-define-key 'normal eshell-mode-map
+    (kbd "C-k") 'eshell-previous-input
+    (kbd "C-j") 'eshell-next-input
+    (kbd "RET") 'eshell-send-input)
+
+  (with-eval-after-load 'org
+    (setq org-mobile-directory "~/Nextcloud/org-mobile/")
+    (setq org-mobile-files (concat org-directory "/mobile/"))
+    (setq org-default-notes-file (concat org-directory "/notes.org"))
+    (setq org-agenda-files (list org-directory))
+    (setq org-agenda-include-diary t))
+
   (load-file "~/.spacemacs.d/mu4e.el")
   ;; (when (file-exists-p custom-file)
   ;;  (load-file custom-file))
@@ -100,6 +117,7 @@ you should place your code here."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(doc-view-resolution 216)
  '(ns-command-modifier (quote control))
  '(paradox-github-token t))
 (custom-set-faces
