66a0e59d9bc9f7b2dcb3346182d2d34c2bfbb9b4
Georgy Kibardin <georgy@kibardin.name>
2024-01-18 14:49:21 +0300

    Toggle group by caps is now in another file (group)

--- bin/setup-keyboard.sh
+++ bin/setup-keyboard.sh
@@ -2,32 +2,34 @@
 
 BUILTIN_KBD_ID=$(xinput --list --id-only 'AT Translated Set 2 keyboard')
 
-# swap CTRL and ALT
-# make "print scrren" RWIN
-xkbcomp -i $BUILTIN_KBD_ID - $DISPLAY <<EOF
-xkb_keymap {
-    xkb_keycodes  {
-        include "evdev+aliases(qwerty)"
-
-        <LALT> = 37;
-        <LCTL> = 64;
-        
-        <RCTL> = 108;
-        <RALT> = 105;
-
-        <INS>  = 255;
-
-        alias <ALGR> = <RALT>;
+if test $? -eq 0
+then
+    # swap CTRL and ALT
+    # make "print scrren" RWIN
+    xkbcomp -i $BUILTIN_KBD_ID - $DISPLAY <<EOF
+    xkb_keymap {
+        xkb_keycodes  {
+            include "evdev+aliases(qwerty)"
+    
+            <LALT> = 37;
+            <LCTL> = 64;
+            
+            <RCTL> = 108;
+            <RALT> = 105;
+    
+            alias <ALGR> = <RALT>;
+        };
+        xkb_types     { include "complete" };
+        xkb_compat    {
+            include "complete"
+            indicator "Caps Lock" { groups = 2; };
+        };
+        xkb_symbols   { include "pc+us+ru:2+inet(evdev)+group(caps_toggle)" };
+        xkb_geometry  { include "pc(pc105)" };
     };
-    xkb_types     { include "complete" };
-    xkb_compat    {
-        include "complete"
-        indicator "Caps Lock" { groups = 2; };
-    };
-    xkb_symbols   { include "pc+us+ru:2+inet(evdev)+capslock(grouplock)" };
-    xkb_geometry  { include "pc(pc105)" };
-};
 EOF
+
+fi
 
 MS_KBD_ID=$(xinput --list --id-only 'Microsoft Microsoft® 2.4GHz Transceiver v9.0')
 
@@ -55,7 +57,7 @@ then
             include "complete"
             indicator "Caps Lock" { groups = 2; };
         };
-        xkb_symbols   { include "pc+us+ru:2+inet(evdev)+capslock(grouplock)" };
+        xkb_symbols   { include "pc+us+ru:2+inet(evdev)+group(caps_toggle)" };
         xkb_geometry  { include "pc(pc105)" };
     };
 EOF
